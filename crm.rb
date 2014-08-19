5fc73bd3f22e1dcdbfb374fd6f159103773b4520
Jamil Velji <jamil@jamilvelji.com>
2014-08-19 15:15:46 -0400

    Finish off edit functionality and view pages

--- crm.rb
+++ crm.rb
@@ -36,9 +36,24 @@ get "/contacts/:id" do
 end
 
 get "/contacts/:id/edit" do
+	@contact = @@rolodex.find(params[:id].to_i)
 	if @contact
 			erb :edit_contact
 		else
 			raise Sinatra::NotFound
 		end
+end
+
+put "/contacts/:id" do
+	@contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+		redirect to("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
 end
