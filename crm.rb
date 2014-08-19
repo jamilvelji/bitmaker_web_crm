1e4a693f883d05d459334a88db0195081daa85a0
Jamil Velji <jamil@jamilvelji.com>
2014-08-19 12:04:46 -0400

    Add new contact

--- crm.rb
+++ crm.rb
@@ -2,18 +2,18 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
+# Initiates the rolodex class
+@@rolodex = Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Jamil", "Velji", "Jamil@jamilvelji.com", "I am awesome")
-	@contacts << Contact.new("Karim", "Velji", "karimvelji@gmail.com", "Brother")
-	@contacts << Contact.new("Tanisara", "Yampraserd", "tanisaray@gmail.com", "Fiance")
 	erb :contacts
 end
 
 get '/contacts/new' do
+	erb :new_contact
 end
